924c1223fb67f38955d0161864fb9bf514ebfd92
Lakshit Raina <77397356+lakshitraina@users.noreply.github.com>
2026-02-19 13:49:40 +0530

    Remove sensitive data from firebase-config.js
    Updated Firebase configuration to remove sensitive information.

--- firebase-config.js
+++ firebase-config.js
@@ -3,18 +3,19 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChang
 import { getDatabase, ref, set, onValue, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDNGXuGVRTtDam0HJgEEDJJzHdmvgXdFbk",
-    authDomain: "studysmart-5a8c9.firebaseapp.com",
-    projectId: "studysmart-5a8c9",
-    storageBucket: "studysmart-5a8c9.firebasestorage.app",
-    messagingSenderId: "371848789071",
-    appId: "1:371848789071:web:49fb9658506d242f933e3a",
-    measurementId: "G-HG3SVFKT05"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 const provider = new GoogleAuthProvider();
+
 
 export { auth, db, provider, signInWithPopup, signOut, onAuthStateChanged, ref, set, onValue, get, child };
